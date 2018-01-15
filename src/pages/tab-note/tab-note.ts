33d1127e45260b6a482bbe58f1fd9cdc84e7dd60
霍微信 <wahid@girtu.com>
2018-01-15 18:33:24 +0800

    tab-note nav-bar上放ion-segment成功，可切换super-tabs，自带下边框，但是不好看，所以去掉了下边框

--- src/pages/tab-note/tab-note.ts
+++ src/pages/tab-note/tab-note.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {
   AlertController,
   IonicPage, MenuController,
   ModalController,
   NavController,
-  NavParams, Platform,
+  NavParams, Platform, Tabs,
 } from 'ionic-angular';
 import {UserService} from "../../providers/user-service";
 import {ControllersService} from "../../providers/controllers-service";
@@ -19,10 +19,10 @@ import {NoteService} from "../../providers/note-service";
   templateUrl: 'tab-note.html',
 })
 export class TabNotePage {
-
   tab1: any = NoteHourTab;
   tab2: any = NoteDayTab;
   tab3: any = NoteDayTab;
+  _currentTabIndex = '0'
 
   constructor(public navCtrl: NavController,
               public superTabsCtrl: SuperTabsController,
@@ -33,5 +33,18 @@ export class TabNotePage {
 
   async ionViewDidLoad() {
     setTimeout(()=>this.superTabsCtrl.enableTabsSwipe(false), 1000)
+  }
+
+  set currentTabIndex(index: string) {
+    this._currentTabIndex = index
+    this.superTabsCtrl.slideTo(parseInt(index))
+  }
+
+  get currentTabIndex() {
+    return this._currentTabIndex
+  }
+
+  changeTabs(tab) {
+
   }
 }
